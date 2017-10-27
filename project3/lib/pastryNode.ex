88df579f0787495d8f64676bc26ab017c9b0c099
Prakhar Mohan Srivastava <prakharmohan@ufl.edu>
2017-10-26 23:31:24 -0400

    Adding code for pastry API

--- project3/lib/pastryNode.ex
+++ project3/lib/pastryNode.ex
@@ -11,14 +11,34 @@ def Pastrynode do
         {:ok,%{:node_id => args}}
     end
 
-    
-
     def longest_prefix_match(key,hash_id,start_value,longest_prefix_count) do
-            if(String.at(key,start_value) == String.at(hash_id,start_value)) do
+        if(String.at(key,start_value) == String.at(hash_id,start_value)) do
             longest_prefix_count=longest_prefix_matched(key,hash_id,start_value+1,longest_prefix_count+1)
-            end
-            longest_prefix_count
         end
+        longest_prefix_count
+    end
+
+    def get_routing_table_entry(key, longest_prefix_count, routing_table) do
+        numRow = longest_prefix_count
+        numCol = Integer.parse(String.at(key, longest_prefix_count+1))
+        if(routing_table[numRow][numCol] != nil) do
+            routing_table[numRow][numCol]
+        end
+    end
+
+    def set_routing_table_entry(entry, longest_prefix_count, routing_table) do
+        numRow = longest_prefix_count
+        numCol = Integer.parse(String.at(entry, longest_prefix_count+1))
+        routing_table_updated = cond do
+            routing_table[numRow][numCol] == nil ->
+                rowMap = routing_table[numRow]
+                rowMap = Map.put(rowMap, numCol, key)
+                routing_table = Map.put(routing_table, numRow, rowMap)
+            true ->
+                routing_table
+            end
+        routing_table_updated
+    end
 
     # fetching nearest node for the last case in pastry
     def searchAllForNearestNeighbour(allSet,key,nodeHash,lngth) do
@@ -106,6 +126,29 @@ def Pastrynode do
 
     def findMatchingSubstrInList(node_list,substring,nodeHash) do
 
+    end
+
+    def handle_cast({:updateNode,leaf_upper,leaf_lower,neighbor_set,routing_table,num_req,num_rows,num_cols},state) do
+        {_,leaf_upper}=Map.get_and_update(state,:leaf_upper, fn current_value -> {current_value,leaf_upper} end)
+        {_,leaf_lower}=Map.get_and_update(state,:leaf_lower, fn current_value -> {current_value,leaf_lower} end)
+        {_,routing_table}=Map.get_and_update(state,:routing_table, fn current_value -> {current_value,routing_table} end)
+        {_,num_req}=Map.get_and_update(state,:num_req, fn current_value -> {current_value,num_req} end)
+        {_,hop_count}=Map.get_and_update(state,:hop_count, fn current_value -> {current_value,0} end)
+        {_,num_rows}=Map.get_and_update(state,:num_rows, fn current_value -> {current_value,num_rows} end)
+        {_,num_cols}=Map.get_and_update(state,:num_cols, fn current_value -> {current_value,num_cols} end)
+
+
+        state=Map.merge(state,leaf_upper)
+        state=Map.merge(state,leaf_lower)
+        state=Map.merge(state, routing_table)
+        state=Map.merge(state, num_req)
+        state=Map.merge(state, hop_count)
+        state=Map.merge(state, num_rows)
+        state=Map.merge(state, num_cols)
+
+        IO.puts "#{inspect self()} #{inspect state}"
+
+        {:noreply,state}
     end
 
 end
